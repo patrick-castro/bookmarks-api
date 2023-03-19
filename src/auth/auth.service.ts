8f3290c499dd0f3a41a5141609beb69c49eb42f1
patrick-castro <castropatrickantonio@gmail.com>
2023-03-19 22:11:45 +0800

    Enable validation Pipes

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
-  signup() {
-    return 'test signup';
+  signup(dto: AuthDto) {
+    return this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash: dto.password,
+      },
+    });
   }
 
   signin() {
