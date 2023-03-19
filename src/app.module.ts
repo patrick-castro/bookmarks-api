fc78d22bcb8bea13f3d602e3cd6fcca36946a066
patrick-castro <castropatrickantonio@gmail.com>
2023-03-19 20:47:47 +0800

    Init modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UserModule, BookmarkModule, AuthModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
