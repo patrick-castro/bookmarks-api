cde718b50e6382f04d8c8a805a0494a8bb7dba7d
patrick-castro <castropatrickantonio@gmail.com>
2023-03-19 22:53:19 +0800

    Integrate jwt strategy

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,18 +1,52 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private config: ConfigService,
+    private jwt: JwtService,
+  ) {}
 
-  signup(dto: AuthDto) {
-    return this.prisma.user.create({
-      data: {
-        email: dto.email,
-        hash: dto.password,
-      },
+  async signToken(userId: number, email: string) {
+    const payload = { sub: userId, email };
+
+    const secret = this.config.get('JWT_SECRET');
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '12h',
+      secret,
     });
+
+    return {
+      access_token: token,
+    };
+  }
+
+  async signup(dto: AuthDto) {
+    try {
+      const hash = await argon.hash(dto.password);
+
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+
+      delete user.hash;
+      return this.signToken(user.id, user.email);
+    } catch (error) {
+      if (error.code === 'P2002') {
+        throw new ForbiddenException('Credentials already taken');
+      }
+
+      throw error;
+    }
   }
 
   signin() {
